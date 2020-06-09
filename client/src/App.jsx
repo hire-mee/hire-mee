3f59a47a8fe4ce438241b64c7901e00f7b5e282c
Alex Benko <benkoalex521@gmail.com>
2020-06-09 13:40:50 -0700

    Set up initial popup for detailed view as well as a function that calculated the difference between a jobs salary and users desired salary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,18 @@ import DetailedView from './components/DetailedView/DetailedView.jsx';
 class App extends React.Component{
   constructor(props){
     super(props);
+
+    this.state = {
+      psuedo: {positionTitle: "Full-Stack Web Developer",salary: 120000, loc: "Mountain View, Ca", urlLink:"www.fake.com", companyName: "Google", descr:"This is a fake job", category:"Applied", submitDate: "06/06/2020", deadLine: "06/20/2020", color: "orange" },
+      desiredSalary: 100000
+    }
   }
 
 
   render(){
     return (
       <div className="main">
-        <DetailedView />
+        <DetailedView jobInfo={this.state.psuedo} desired={this.state.desiredSalary}/>
       </div>
     );
   }
--- client/src/components/DetailedView/DetailedView.jsx
+++ client/src/components/DetailedView/DetailedView.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
+let calculateSalaryDifference = (jobSalary,desiredSalary)=>{
 
-class DetailedView extends React.Component{
-  constructor(props){
-    super(props);
-  }
+  let numerator = Math.abs(jobSalary - desiredSalary);
+  let denominator = (jobSalary + desiredSalary)/2;
+  let result = numerator/denominator;
 
-  render(){
-    return (
-      <div className="detailed-view">
-        DetailedView
-      </div>
-    )
-  }
+  return Math.round(result * 100);
+};
+
+function DetailedView({jobInfo, desired}) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div style={{height:"90%",width:"90%"}}>
+      <Button variant="primary" onClick={() => setShow(true)}>
+        Custom Width Modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="detailed-view"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            <h1 >{jobInfo.positionTitle}</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="popupholder">
+            <div className = "popupholder-left">
+              <h1 style={{color: "rgb(56, 182, 255)"}}>{jobInfo.companyName}</h1><br />
+              Salary: <br />
+              {jobInfo.salary} <br />
+              Desired {calculateSalaryDifference(jobInfo.salary,desired) + ' %'} <br/>
+              Submitted: <br />
+              {jobInfo.submitDate} <br />
+              Deadline: <br />
+              {jobInfo.deadLine} <br />
+              Location: <br />
+              {jobInfo.loc}
+            </div>
+            <div className = "popupholder-right">
+
+            </div>
+          </div>
+
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
+
 
 export default DetailedView;
