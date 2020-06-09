89745d37e2b415ac01b532cf2ea64ed1654e6731
Alex Benko <benkoalex521@gmail.com>
2020-06-09 13:49:43 -0700

    Improved styling for the job title

--- client/src/components/DetailedView/DetailedView.jsx
+++ client/src/components/DetailedView/DetailedView.jsx
@@ -11,8 +11,18 @@ let calculateSalaryDifference = (jobSalary,desiredSalary)=>{
   return Math.round(result * 100);
 };
 
-function DetailedView({jobInfo, desired}) {
+let DetailedView = ({jobInfo, desired}) => {
   const [show, setShow] = useState(false);
+  const style = {};
+
+  style.jobTitle = {
+    fontWeight:"400",
+    fontStyle: "normal",
+    color:"rgb(82, 89, 112)",
+    textDecoration:"none",
+    lineHeight: "1.4",
+    fontFamily: "YACkoM60Ufo 0, _fb_, auto"
+  };
 
   return (
     <div style={{height:"90%",width:"90%"}}>
@@ -28,13 +38,13 @@ function DetailedView({jobInfo, desired}) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-custom-modal-styling-title">
-            <h1 >{jobInfo.positionTitle}</h1>
+            <h1 style={style.jobTitle} >{jobInfo.positionTitle}</h1>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="popupholder">
             <div className = "popupholder-left">
-              <h1 style={{color: "rgb(56, 182, 255)"}}>{jobInfo.companyName}</h1><br />
+              <h1 >{jobInfo.companyName}</h1><br />
               Salary: <br />
               {jobInfo.salary} <br />
               Desired {calculateSalaryDifference(jobInfo.salary,desired) + ' %'} <br/>
