2670122df49036fb773beb65231eb434b2c5e359
Alex Benko <benkoalex521@gmail.com>
2020-06-09 14:25:46 -0700

    Added conditional styling for salary

--- client/src/components/DetailedView/DetailedView.jsx
+++ client/src/components/DetailedView/DetailedView.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-let calculateSalaryDifference = (jobSalary,desiredSalary)=>{
+let calculateSalaryDifference = (desiredSalary,jobSalary)=>{
+  let difference = 100 * Math.abs((desiredSalary - jobSalary)/((desiredSalary+jobSalary)/2));
 
-  let numerator = Math.abs(jobSalary - desiredSalary);
-  let denominator = (jobSalary + desiredSalary)/2;
-  let result = numerator/denominator;
+  return  Math.round(difference);
+};
 
-  return Math.round(result * 100);
+let plusOrMinus = (desiredSalary,jobSalary) =>{
+  if(desiredSalary < jobSalary){
+    return '+'
+  } else {
+    return '-'
+  }
 };
 
 let DetailedView = ({jobInfo, desired}) => {
@@ -24,8 +29,24 @@ let DetailedView = ({jobInfo, desired}) => {
     fontFamily: "YACkoM60Ufo 0, _fb_, auto"
   };
 
+  style.company = {
+    fontWeight: "400",
+    fontStyle: "normal",
+    color: "rgb(56, 182, 255)",
+    textDecoration: "none",
+    lineHeight: "1.4",
+    fontFamily: "YACkoA9eHeY 0, _fb_, auto",
+    fontSize: "42.6667px"
+  };
+
+  style.desired = {
+    fontWeight: "700",
+    fontStyle: "normal",
+    color: desired < jobInfo.salary ? "rgb(32, 187, 136)" : "rgb(220,20,60)"
+  }
+
   return (
-    <div style={{height:"90%",width:"90%"}}>
+    <div >
       <Button variant="primary" onClick={() => setShow(true)}>
         Custom Width Modal
       </Button>
@@ -37,17 +58,19 @@ let DetailedView = ({jobInfo, desired}) => {
         aria-labelledby="example-custom-modal-styling-title"
       >
         <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
+          <Modal.Title id="example-custom-modal-styling-title" style={{paddingLeft:"50px"}}>
             <h1 style={style.jobTitle} >{jobInfo.positionTitle}</h1>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="popupholder">
+          <div className="popupholder" style={{paddingLeft: "60px",paddingBottom:"100px"}}>
             <div className = "popupholder-left">
-              <h1 >{jobInfo.companyName}</h1><br />
+              <h1 style={style.company}>{jobInfo.companyName}</h1><br />
               Salary: <br />
-              {jobInfo.salary} <br />
-              Desired {calculateSalaryDifference(jobInfo.salary,desired) + ' %'} <br/>
+              ${jobInfo.salary} <br />
+              <div style={style.desired}>
+                Desired {plusOrMinus(desired,jobInfo.salary) + calculateSalaryDifference(desired,jobInfo.salary) + ' %'} <br/>
+              </div>
               Submitted: <br />
               {jobInfo.submitDate} <br />
               Deadline: <br />
