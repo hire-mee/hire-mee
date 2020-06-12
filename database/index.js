e89ab353365884560acf80ee03e3503fd8e625ba
Michael Siu <michaeltsiu13@gmail.com>
2020-06-12 11:45:33 -0700

    Added css and fixed typo in statistics rendering

--- database/index.js
+++ database/index.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: 'postgres',
-  password: '',
-  host: 'localhost',
+  // password: '',
+  // host: 'localhost',
   port: 5432,
   database: "hiremee"
 })
