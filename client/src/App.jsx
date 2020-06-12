bd31aa488690ac2fc32ec6e390940687dbd2f2fa
Danny (vai) San <vai.quoc.san@gmail.com>
2020-06-12 11:12:37 -0700

    added profile avatar and name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './components/SignUp/SignUp.jsx';
 import Statistics from './components/Statistics/Statistics.jsx';
 import { Briefcase, GraphUp, Trophy, GeoAlt, PersonFill, GearFill, PauseFill, ArrowClockwise, BoxArrowRight } from 'react-bootstrap-icons';
 import Jobs from './components/jobs/Jobs.jsx'
+import Profile from './components/Profile/Profile.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -60,7 +61,7 @@ class App extends React.Component {
             </div>
             <div className="Header">
               <div className="Header-title">{this.state.page}</div>
-              <div className="Profile-area">PROFILE AREA</div>
+              <div className="Profile-area"><Profile userData={this.state.currentUser}/></div>
             </div>
             <div className="Display">{this.componentHandler()}</div>
           </div>
---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    console.log('profile area', this.props);
+    return (
+      <div>
+        <div className="userProfileContainer">
+          <div className="userAvatar"><img className="userAvatar" src="https://www.w3schools.com/howto/img_avatar.png"></img></div>
+          {}
+          <div className="userProfileData">
+            {this.props.userData.firstname}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
