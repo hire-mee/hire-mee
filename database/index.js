bee3f38012cf76d788b715db3d41d3ab1695a3c3
Alex Benko <benkoalex521@gmail.com>
2020-06-12 18:06:21 -0700

    Simplified

--- database/index.js
+++ database/index.js
@@ -6,17 +6,11 @@ const pool = new Pool({
   database: "hiremee"
 })
 
-<<<<<<< HEAD
 pool.connect()
 .then(console.log('Connectd to psql!'))
 .catch((err)=>console.log(err))
 
 
-=======
-pool.query('select now()', () => {
-  console.log('Connected to psql!');
-});
->>>>>>> 71086db919b0376423702c260495696115072198
 
 
 module.exports = pool;
