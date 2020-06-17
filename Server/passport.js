266ef79a3e45a16336bd69eab133c1bfbd4e4432
Michael Siu <michaeltsiu13@gmail.com>
2020-06-16 17:21:47 -0700

    random

---
+++ Server/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport'); // declare variable for passport
+const LocalStrategy = require('passport-local').Strategy; // localization of passport auth
+const connection = require('../database/index.js'); // database connection
+const validatePassword = require('./lib/passwordUtils.js').validatePassword;
+
+let customFields = {
+    usernameField: 'email',
+    passwordField: 'pass'
+}
+
+let verifyCallback = (email, pass, done) => {
+    let queryStr = `SELECT * from userinfo WHERE email = '${email}';`
+    connection.query(queryStr, function(err, user) {
+      if (err) { 
+          return done(err); 
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      const isValid = validatePassword(pass, user.rows[0].pass, user.rows[0].salt); // verifies input attempt with hash in DB
+
+      if (isValid) {
+        return done(null, user); // success case based on validPassword
+      } else {
+        return done(null, false); // fail case based on validPassword
+      }
+    });
+}
+
+let strategy = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
+
+// serialization of sessions
+
+passport.serializeUser((user, done) => {
+    console.log('SerializeUser function called.');
+    
+    done(null, user.rows[0].email)
+})
+
+passport.deserializeUser((email, callback) => {
+    connection.query('SELECT id, email FROM userinfo WHERE email = $1', [email], (err, results) => {
+      if(err) {
+        console.log('Error when selecting user on session deserialize', err)
+        return callback(err)
+      }
+  
+      callback(null, results.rows[0])
+    })
+  })
---
+++ client/src/components/LogIn/Login.jsx
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import axios from "axios"
+import { PersonFill } from 'react-bootstrap-icons';
+import { Envelope } from 'react-bootstrap-icons';
+import { Lock } from 'react-bootstrap-icons';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        email: '',
+        pass: '',
+    };
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  handleLogin(e){
+    e.preventDefault();
+    axios.post('/api/login', {
+<<<<<<< HEAD
+        email: this.state.email,
+        pass: this.state.pass
+    })
+    .then(result => {
+      axios.get(`api/user/${result.data}`)
+      .then(innerResults => this.props.storeUserData(innerResults.data[0]))
+      .catch(innerErr => console.error(innerErr))
+    })
+    .then(() => this.props.changePage('page', 'Jobs'))
+    .catch(() => (location.reload()))
+    // .catch(err => window.alert("error with handling login"))
+=======
+      email: this.state.email,
+      pass: this.state.pass
+    })
+    .then(() => {
+      axios.get(`/api/email/${this.state.email}`)
+      .then(innerResults => this.props.storeUserData(innerResults.data[0]))
+      .catch(() =>window.alert("Error with login, please check your email & password"))
+    })
+    .then(() => this.props.changePage('page', 'Statistics'))
+    .catch(() => {location.reload(); window.alert("Error with login, please check your email & password")})
+>>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
+  }
+
+  inputChangeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    }, ()=>console.log(this.state));
+  }
+
+  render() {
+      return (
+        <div className="signup_main_container">
+        <div className="login_main_title" onClick={() => this.props.changePage('page', 'Signup')}>Hire-Mee</div>
+        <div id="signup_gist">Better than your own Excel Sheet.</div>
+        <div id="signup_start_here"> Welcome back!</div>
+          <div className="sign_up_input_container">
+              <div className="sign_up_create_new_account_text">Log in to continue.</div>
+<<<<<<< HEAD
+  
+                <div className="signup_input_form_container">
+=======
+
+                <div className="signup_input_form_container">
+
+                    <div className="signup_google_container">
+                    <img id="signup_google_icon" src="https://cdn.worldvectorlogo.com/logos/google-icon.svg"></img>
+                    <div id="signup_google_text">Log in with Google</div>
+                  </div>
+      
+                  <div className="signup_or_separator_container">
+                    <hr className="signup_or_hr_tag"/>
+                    <div>or</div>
+                    <hr className="signup_or_hr_tag"/>
+                  </div>
+
+
+>>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
+                  <form onSubmit={this.submitHandler}>
+                    <div className="signup_input_icon_div">
+                      <Envelope className="signup_bootstrap_icon"/>
+                      <input
+                        onChange={this.inputChangeHandler}
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        required
+                      ></input>
+                    </div>
+                    <div className="signup_input_icon_div">
+                    <Lock className="signup_bootstrap_icon"/>
+                      <input
+                        onChange={this.inputChangeHandler}
+                        type="password"
+                        name="pass"
+                        placeholder="Password"
+                        required
+                      ></input>
+                    </div>
+<<<<<<< HEAD
+
+                    <div className="signup_button_container">
+                    <button id="signup_signup_button" onClick={this.handleLogin}>Login</button>
+                  </div>
+
+                  <div className="signup_already_signedup_container">
+                  <div id="signup_already_signedup_text">Click here to sign up.</div>
+                  <div id="signup_already_signedup_button" onClick={() => this.props.changePage('page', 'Signup')}>Sign Up</div>
+                </div>
+                  </form>
+                </div>
+
+=======
+                    <div className="signup_button_container">
+                    <button id="signup_signup_button" onClick={this.handleLogin}>Login</button>
+                  </div>
+                  <div className="signup_already_signedup_container">
+                  <div id="signup_already_signedup_text">Click here to sign up.</div>
+                  <div id="signup_already_signedup_button" onClick={()=>this.props.changePage('page', 'Signup')}>Sign Up</div>
+                </div>
+                  </form>
+                </div>
+>>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
+          </div>
+        </div>
+      );
+  }
+}
+<<<<<<< HEAD
+// commented out for when Google auth is correctly implemented
+// 
+// <div className="signup_google_container">
+//                 <img id="signup_google_icon" src="https://cdn.worldvectorlogo.com/logos/google-icon.svg"></img>
+//                 <div id="signup_google_text">Continue with Google</div>
+//               </div>
+  
+//               <div className="signup_or_separator_container">
+//                 <hr className="signup_or_hr_tag"/>
+//                 <div>or</div>
+//                 <hr className="signup_or_hr_tag"/>
+//               </div>
+=======
+
+
+// background: white;
+//     padding: 3% 6%;
+//     border-radius: 4px;
+//     margin-left: auto;
+//     margin-right: auto;
+//     margin-top: 6%;
+//     cursor: pointer;
+//     display: flex;
+>>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
