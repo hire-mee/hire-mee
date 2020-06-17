f76bd66e7301c4c82d44b800950c282739305460
Michael Siu <michaeltsiu13@gmail.com>
2020-06-16 17:56:23 -0700

    merge master to my branch

--- client/src/components/LogIn/Login.jsx
+++ client/src/components/LogIn/Login.jsx
@@ -18,22 +18,6 @@ export default class Login extends Component {
   handleLogin(e){
     e.preventDefault();
     axios.post('/api/login', {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        email: this.state.email,
-        pass: this.state.pass
-    })
-    .then(result => {
-      axios.get(`api/user/${result.data}`)
-      .then(innerResults => this.props.storeUserData(innerResults.data[0]))
-      .catch(innerErr => console.error(innerErr))
-    })
-    .then(() => this.props.changePage('page', 'Jobs'))
-    .catch(() => (location.reload()))
-    // .catch(err => window.alert("error with handling login"))
-=======
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
       email: this.state.email,
       pass: this.state.pass
     })
@@ -44,10 +28,6 @@ export default class Login extends Component {
     })
     .then(() => this.props.changePage('page', 'Statistics'))
     .catch(() => {location.reload(); window.alert("Error with login, please check your email & password")})
-<<<<<<< HEAD
->>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
   }
 
   inputChangeHandler(e) {
@@ -64,13 +44,6 @@ export default class Login extends Component {
         <div id="signup_start_here"> Welcome back!</div>
           <div className="sign_up_input_container">
               <div className="sign_up_create_new_account_text">Log in to continue.</div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-  
-                <div className="signup_input_form_container">
-=======
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
 
                 <div className="signup_input_form_container">
 
@@ -78,7 +51,7 @@ export default class Login extends Component {
                     <img id="signup_google_icon" src="https://cdn.worldvectorlogo.com/logos/google-icon.svg"></img>
                     <div id="signup_google_text">Log in with Google</div>
                   </div>
-      
+
                   <div className="signup_or_separator_container">
                     <hr className="signup_or_hr_tag"/>
                     <div>or</div>
@@ -86,10 +59,6 @@ export default class Login extends Component {
                   </div>
 
 
-<<<<<<< HEAD
->>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
                   <form onSubmit={this.submitHandler}>
                     <div className="signup_input_icon_div">
                       <Envelope className="signup_bootstrap_icon"/>
@@ -111,23 +80,6 @@ export default class Login extends Component {
                         required
                       ></input>
                     </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-                    <div className="signup_button_container">
-                    <button id="signup_signup_button" onClick={this.handleLogin}>Login</button>
-                  </div>
-
-                  <div className="signup_already_signedup_container">
-                  <div id="signup_already_signedup_text">Click here to sign up.</div>
-                  <div id="signup_already_signedup_button" onClick={() => this.props.changePage('page', 'Signup')}>Sign Up</div>
-                </div>
-                  </form>
-                </div>
-
-=======
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
                     <div className="signup_button_container">
                     <button id="signup_signup_button" onClick={this.handleLogin}>Login</button>
                   </div>
@@ -137,32 +89,11 @@ export default class Login extends Component {
                 </div>
                   </form>
                 </div>
-<<<<<<< HEAD
->>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
           </div>
         </div>
       );
   }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-// commented out for when Google auth is correctly implemented
-// 
-// <div className="signup_google_container">
-//                 <img id="signup_google_icon" src="https://cdn.worldvectorlogo.com/logos/google-icon.svg"></img>
-//                 <div id="signup_google_text">Continue with Google</div>
-//               </div>
-  
-//               <div className="signup_or_separator_container">
-//                 <hr className="signup_or_hr_tag"/>
-//                 <div>or</div>
-//                 <hr className="signup_or_hr_tag"/>
-//               </div>
-=======
-=======
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
 
 
 // background: white;
@@ -172,9 +103,4 @@ export default class Login extends Component {
 //     margin-right: auto;
 //     margin-top: 6%;
 //     cursor: pointer;
-<<<<<<< HEAD
 //     display: flex;
->>>>>>> 333e06b5a5abf08554eb819cb645904cb592c937
-=======
-//     display: flex;
->>>>>>> 51944d10f2f4485b82693ecabeaf7a1329cb5ce3
