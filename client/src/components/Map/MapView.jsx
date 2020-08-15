1af9676c2cb637235770daac0c5c534a077b82ad
Bertrand Shao <shaob@g.ucla.edu>
2020-08-15 16:40:30 -0700

    Delete node modules

--- client/src/components/Map/MapView.jsx
+++ client/src/components/Map/MapView.jsx
@@ -164,7 +164,6 @@ export class MapView extends Component {
             Set
           </Button>
         </div>
-
         <div>
           <WrappedMap
             googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
