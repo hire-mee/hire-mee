963d3e98aced1be9455b0ee9c696d4c80b21a5ee
justfrankie <zhuangfrankie@gmail.com>
2020-08-19 23:56:28 -0700

    properly added one feature view

--- client/src/components/Demo/Demo.jsx
+++ client/src/components/Demo/Demo.jsx
@@ -7,22 +7,22 @@ import {
 
   export default function Demo() {
     return (
-      <div>
+      <div className="demo_body_container">
 
         <div className="demo_top_container">
-        <nav className="demo_nav_container">
-            <ul className="demo_nav_links_ul">
-              <li className="demo_nav_links">
-                <Link to="/">ABOUT</Link>
-              </li>
-              <li className="demo_nav_links">
-                <Link to="/signup">SIGNUP</Link>
-              </li>
-              <li className="demo_nav_links">
-                <Link to="/login">LOGIN</Link>
-              </li>
-            </ul>
-          </nav>
+          <nav className="demo_nav_container">
+              <ul className="demo_nav_links_ul">
+                <li className="demo_nav_links">
+                  <Link to="/">ABOUT</Link>
+                </li>
+                <li className="demo_nav_links">
+                  <Link to="/signup">SIGNUP</Link>
+                </li>
+                <li className="demo_nav_links">
+                  <Link to="/login">LOGIN</Link>
+                </li>
+              </ul>
+            </nav>
           </div>
 
           <div className="demo_header_background_wrapper">
@@ -37,12 +37,36 @@ import {
                     <div id="demo_header_about_text">of organizing job apps.</div>
                     <div className="demo_pink_button_container">
                       <button className="demo_pink_button">Start here</button>
-                      </div>
+                    </div>
                 </div>
 
                 </div>
               </div>
             </div>
+
+            <div className="demo_rightside_feature_container">
+              <div className="demo_rightside_feature_desc_container">
+
+                  <div className="demo_rightside_flower_container">
+                    <img src="https://hire-mee-assets.s3-us-west-1.amazonaws.com/flower1.png" className="demo_rightside_fower"></img>
+                  </div>
+
+                  <div className="demo_rightside_feature_subcontainer">
+                    <div className="demo_rightside_feature_heading_text">Minimal, modern UI.</div>
+                    <div className="demo_rightside_feature_heading_subtext">Clean and easy-to-use</div>
+                    <div className="demo_rightside_feature_heading_subtext">dashboard view for all of</div>
+                    <div className="demo_rightside_feature_heading_subtext">your tracked applications.</div>
+                  </div>
+
+              </div>
+
+              <div className="demo_rightside_feature_static_image_container">
+
+                <img className="demo_rightside_feature_image" src="https://hire-mee-assets.s3-us-west-1.amazonaws.com/job-view.png"></img>
+              </div>
+
+            </div>
+            
       </div>
     )
   }
